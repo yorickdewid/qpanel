40fba5bd2ed73a9fb2f8c67e712ddff453e2afec
Yorick de Wid <yorickdewid@users.noreply.github.com>
2015-03-16 22:09:25 +0100

    Create db.php

---
+++ cnf/db.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Database configuration file.
+ * @package qpanel
+ * @subpackage core -> config
+ * @author Bobby Allen (ballen@bobbyallen.me)
+ * @copyright ZPanel Project (http://www.zpanelcp.com/)
+ * @link http://www.zpanelcp.com/
+ * @license GPL (http://www.gnu.org/licenses/gpl.html)
+ */
+$host = 'localhost';
+$dbname = 'zpanel_core';
+$user = 'root';
+$pass = 'YOUR_ROOT_MYSQL_PASSWORD';
